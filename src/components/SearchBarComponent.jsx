db50a3317e61a64a8ca88ed07bbe2d5453a8e2ac
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-04-14 11:09:47 +0200

    some HTML is in the component

--- src/components/SearchBarComponent.jsx
+++ src/components/SearchBarComponent.jsx
@@ -1,49 +1,28 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styles/SearchBarComponent.css';
-// import SearchBar from 'react-search-bar';
-import Autosuggest from 'react-autosuggest';
 
-// NATIVE HTML SEARCHBAR:
-////////////////////////////////////////
 export default class SearchBarComponent extends Component {
   render () {
     return (
-      <input type="text"
-             defaultValue="Utrecht"
-             className={styles.foobar} />
+
+      <div className={styles["searchbar-container"]}>
+        <input
+          className={styles['searchbar-btn-menu']}
+          type="button"
+          value="0"
+        />
+        <input
+          className={styles['searchbar-btn-search']}
+          type="button"
+          value="+"
+        />
+        <input
+          type="text"
+          defaultValue="Utrecht"
+          className={styles["searchbar"]}
+        />
+      </div>
     );
   }
 }
-
-
-// Using package "react-search-bar" (=deprecated)
-//////////////////////////////////////////////////////////////
-// export default class SearchBarComponent extends Component {
-
-//   handleChange() {
-//     console.log("[dbg] detected change in searchbar!");
-//   }
-
-//   handleSuggestions() {
-//     return ["Bilthoven", "De Bilt", "Maarssen", "Nieuwegein"];
-//   }
-
-//   onSearch() {
-//     console.log("[dbg] initiate");
-//   }
-
-//   render() {
-//     return (
-//       <SearchBar className={styles.foobar}
-//                  onChange={this.handleChange}
-//                  onSearch={this.onSearch}
-//                  suggestions={this.handleSuggestions()} />
-//     );
-//   }
-// }
-
-// Using package "react-autosuggest" (=OK!)
-//////////////////////////////////////////////////////////////
-// TODO: Vrijdag (14-04-2017) ochtend...
-
