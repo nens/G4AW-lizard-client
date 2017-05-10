392aefc0f5c3230c6f65df351fd06306d9a062d0
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-05-10 14:50:41 +0200

    fix(HMR): Use Hot Module Replacement API with AppContainer and module.hot check.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import ReactDOM from 'react-dom';
-import { app } from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppContainer } from "react-hot-loader";
+import { theStore } from "./store/Store";
+import { Provider } from "react-redux";
+import App from "./components/App";
 
-ReactDOM.render(app, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={theStore}>
+        <Router>
+          <Route path="/" component={Component} />
+        </Router>
+      </Provider>
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
+
+render(App);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    render(require("./components/App").default);
+  });
+}
