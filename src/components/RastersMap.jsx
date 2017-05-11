13ca0094751369216ce66cbf478e5b7b4845fa80
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-05-11 14:01:20 +0200

    Changed uuid of dem to the uuid on staging server.

--- src/components/RastersMap.jsx
+++ src/components/RastersMap.jsx
@@ -1,12 +1,12 @@
-import { connect } from 'react-redux';
-import React from 'react';
-import { Map, TileLayer, WMSTileLayer } from 'react-leaflet';
-import ReactDOM from 'react-dom';
+import { connect } from "react-redux";
+import React from "react";
+import { Map, TileLayer, WMSTileLayer } from "react-leaflet";
+import ReactDOM from "react-dom";
 
-import { VIEWPORT_PADDING } from './Constants';
-import { getRaster } from '../actions/RasterActions';
+import { VIEWPORT_PADDING } from "./Constants";
+import { getRaster } from "../actions/RasterActions";
 
-const hoogteUuid = "10415ccb-ec31-4d43-bdb3-db597061527b";
+const hoogteUuid = "e9ed5725-d94a-4bcb-9dde-5d655da0070e";
 
 class RastersMapComponent extends React.Component {
   componentDidMount() {
@@ -14,11 +14,11 @@ class RastersMapComponent extends React.Component {
   }
 
   getWidth() {
-    return ((this.props.width || window.innerWidth) - VIEWPORT_PADDING) + "px";
+    return (this.props.width || window.innerWidth) - VIEWPORT_PADDING + "px";
   }
 
   getHeight() {
-    return ((this.props.height || window.innerHeight) - VIEWPORT_PADDING) + "px";
+    return (this.props.height || window.innerHeight) - VIEWPORT_PADDING + "px";
   }
 
   render() {
@@ -26,22 +26,24 @@ class RastersMapComponent extends React.Component {
 
     return (
       <div>
-        <Map center={[52.092876, 5.104480]}
-             zoom={13}
-             zoomControl={false}
-             style={{width: this.getWidth(), height: this.getHeight()}}>
+        <Map
+          center={[52.092876, 5.104480]}
+          zoom={13}
+          zoomControl={false}
+          style={{ width: this.getWidth(), height: this.getHeight() }}
+        >
           <TileLayer
-             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
-          {visibleRasters.map((raster) => (
+          {visibleRasters.map(raster => (
             <WMSTileLayer
-              url={raster.wms_info.get('endpoint')}
+              url={raster.wms_info.get("endpoint")}
               key={raster.uuid}
-              layers={raster.wms_info.get('layer')}
-              styles={raster.options.get('styles')}
+              layers={raster.wms_info.get("layer")}
+              styles={raster.options.get("styles")}
             />
-           ))}
+          ))}
         </Map>
       </div>
     );
@@ -50,19 +52,20 @@ class RastersMapComponent extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    'visibleRasters': Object.values(state.rasters)
-      .filter((raster) => !!raster.data)
-      .map((raster) => raster.data)
+    visibleRasters: Object.values(state.rasters)
+      .filter(raster => !!raster.data)
+      .map(raster => raster.data)
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    getRaster: (uuid) => getRaster(uuid, dispatch)
+    getRaster: uuid => getRaster(uuid, dispatch)
   };
 }
 
-const RastersMap = connect(
-  mapStateToProps, mapDispatchToProps)(RastersMapComponent);
+const RastersMap = connect(mapStateToProps, mapDispatchToProps)(
+  RastersMapComponent
+);
 
 export default RastersMap;
