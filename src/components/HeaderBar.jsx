6a16edf5491b8e8245996391d91776f269238662
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-05-23 11:05:38 +0200

    fix(HeaderBar): Adds HeaderBar component.

---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import styles from "./styles/HeaderBar.css";
+
+// A HeaderBar component.
+
+class HeaderBar extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  componentDidMount() {}
+  render() {
+    const { title, icon, handleClick } = this.props;
+    return (
+      <div className={styles.HeaderBar}>
+        {title}
+        <div className={styles.ActionButton} onClick={handleClick}>
+          <i className={`${styles.ActionButtonIcon} material-icons`}>{icon}</i>
+        </div>
+      </div>
+    );
+  }
+}
+
+HeaderBar.propTypes = {
+  handleClick: PropTypes.func,
+  icon: PropTypes.string,
+  title: PropTypes.string
+};
+
+export default HeaderBar;
