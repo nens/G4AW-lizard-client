9b3396da51d1af3249e8745f075c79f73e0d1c77
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-05-23 11:07:06 +0200

    fix(index.js): HMR accept all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { AppContainer } from "react-hot-loader";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import { Provider } from "react-redux";
-import { Route } from "react-router";
 import { theStore } from "./store/Store";
 
 import App from "./components/App";
+import MapSearchView from "./components/MapSearchView";
 import i18n from "./i18n"; // initialized i18next instance
 import React from "react";
 import ReactDOM from "react-dom";
@@ -17,26 +17,23 @@ if (typeof Object.values !== "function") {
   Object.values = valuesES2017;
 }
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <I18nextProvider i18n={i18n}>
-        <Provider store={theStore}>
-          <Router>
-            <Route path="/" component={Component} />
-          </Router>
-        </Provider>
-      </I18nextProvider>
-    </AppContainer>,
-    document.getElementById("root")
-  );
-};
-
-render(App);
+ReactDOM.render(
+  <AppContainer>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={theStore}>
+        <Router basename="/">
+          <div>
+            <Route exact path="/" component={App} />
+            <Route path="/map/:z/:x/:y" component={MapSearchView} />
+          </div>
+        </Router>
+      </Provider>
+    </I18nextProvider>
+  </AppContainer>,
+  document.getElementById("root")
+);
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept("./components/App", () => {
-    render(require("./components/App").default);
-  });
+  module.hot.accept();
 }
