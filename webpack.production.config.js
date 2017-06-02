a9e7928f375e2b9ff35aab8b8029934c2aa19c7e
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-02 09:27:20 +0200

    fix(Webpack): Adds production config which omits things like HMR.

---
+++ webpack.production.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const libraryName = "Lizard";
+
+const definePlugin = new webpack.DefinePlugin({
+  "process.env": {
+    NODE_ENV: '"production"'
+  }
+});
+
+const config = {
+  context: path.join(__dirname, "src"),
+  entry: [__dirname + "/src/index.js"],
+  devtool: false,
+  output: {
+    path: __dirname + "/lib",
+    filename: libraryName + ".js",
+    publicPath: "/scripts/",
+    library: libraryName,
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader?modules", "postcss-loader"]
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.js$/,
+        loader: "eslint-loader",
+        exclude: /node_modules|LizardApiClient/
+      },
+      {
+        test: /\.(png|jpg|svg|woff|eot|ttf|otf)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]
+};
+
+module.exports = config;
