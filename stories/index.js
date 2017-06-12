64fac3e2cba558fe660657eb6a0cfb99fbecc28e
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-12 15:09:55 +0200

    fix(FooterBar): Adds FooterBar component and its storybook.

--- stories/index.js
+++ stories/index.js
@@ -1,75 +1,54 @@
-import runModal from "./ModalStories";
-runModal();
-
+import runCollapsibleBar from "./CollapsibleBarStories";
 import runDetailViewHeader from "./DetailViewHeaderStories";
-runDetailViewHeader();
-
 import runDetailViewSection from "./DetailViewSectionStories";
-runDetailViewSection();
-
-import runTabBar from "./TabBarStories";
-runTabBar();
-
+import runDetailViewTable from "./DetailViewTableStories";
+import runDetailViewTableSection from "./DetailViewTableSectionStories";
 import runDetailViewThumbnails from "./DetailViewThumbnailsStories";
-runDetailViewThumbnails();
-
 import runDetailViewThumbnailsSection
   from "./DetailViewThumbnailsSectionStories";
-runDetailViewThumbnailsSection();
-
-import runDetailViewTable from "./DetailViewTableStories";
-runDetailViewTable();
-
-import runDetailViewTableSection from "./DetailViewTableSectionStories";
-runDetailViewTableSection();
-
-import runCollapsibleBar from "./CollapsibleBarStories";
-runCollapsibleBar();
-
 import runFlatButton from "./FlatButtonStories";
-runFlatButton();
-
+import runFooterBar from "./FooterBarStories";
 import runHeaderBar from "./HeaderBarStories";
-runHeaderBar();
-
 import runInputField from "./InputFieldStories";
-runInputField();
-
 import runLayerSelection from "./LayerSelectionStories";
-runLayerSelection();
-
 import runLegend from "./LegendStories";
-runLegend();
-
 import runListSearchView from "./ListSearchViewStories";
-runListSearchView();
-
 import runLoginLogoutButton from "./LoginLogoutButtonStories";
-runLoginLogoutButton();
-
+import runModal from "./ModalStories";
 import runPhotoView from "./PhotoViewStories";
-runPhotoView();
-
 import runRaisedButton from "./RaisedButtonStories";
-runRaisedButton();
-
-import runSearchResultCard from "./SearchResultCardStories";
-runSearchResultCard();
-
 import runSearchBar from "./SearchBarStories";
-runSearchBar();
-
+import runSearchResultCard from "./SearchResultCardStories";
 import runSimpleLineChart from "./SimpleLineChartStories";
-runSimpleLineChart();
-
 import runSnackBar from "./SnackBarStories";
-runSnackBar();
-
+import runTabBar from "./TabBarStories";
 import runTimeseriesChart from "./TimeseriesChartStories";
-runTimeseriesChart();
-
 import runToggleSwitch from "./ToggleSwitchStories";
-runToggleSwitch();
-
 import runViewSwitchButton from "./ViewSwitchButtonStories";
+
+runCollapsibleBar();
+runDetailViewHeader();
+runDetailViewSection();
+runDetailViewTable();
+runDetailViewTableSection();
+runDetailViewThumbnails();
+runDetailViewThumbnailsSection();
+runFlatButton();
+runFooterBar();
+runHeaderBar();
+runInputField();
+runLayerSelection();
+runLegend();
+runListSearchView();
+runLoginLogoutButton();
+runModal();
+runPhotoView();
+runRaisedButton();
+runSearchBar();
+runSearchResultCard();
+runSimpleLineChart();
+runSnackBar();
+runTabBar();
+runTimeseriesChart();
+runToggleSwitch();
 runViewSwitchButton();
