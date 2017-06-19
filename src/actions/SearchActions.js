4168ff5865b89e7b2884e2df9f8428ba9b2961ab
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-19 11:38:35 +0200

    fix(SearchActions): Adds an action setSearchInputText().

--- src/actions/SearchActions.js
+++ src/actions/SearchActions.js
@@ -1,7 +1,8 @@
 import {
-  START_SEARCH,
+  CLEAR_SEARCH_RESULTS,
   RECEIVE_SEARCH_RESULTS,
-  CLEAR_SEARCH_RESULTS
+  SET_SEARCH_INPUT_TEXT,
+  START_SEARCH
 } from "../constants/ActionTypes";
 import { theStore } from "../store/Store";
 
@@ -14,11 +15,16 @@ export const startSearch = () => ({
 
 export const receiveResults = results => ({
   type: RECEIVE_SEARCH_RESULTS,
-  results: results
+  results
 });
 
 export const clearResults = () => ({
   type: CLEAR_SEARCH_RESULTS
+});
+
+export const setSearchInputText = inputText => ({
+  type: SET_SEARCH_INPUT_TEXT,
+  inputText: inputText
 });
 
 function doSearch(dispatch, q, types = null, exclude = []) {
