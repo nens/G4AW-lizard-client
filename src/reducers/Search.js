9d6bd81d08ae2a342cd6275f73a312f5cfc5097e
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-19 11:36:35 +0200

    fix(): Adds reducer SET_SEARCH_INPUT_TEXT and adds Object.assign() to each reducer (!)

--- src/reducers/Search.js
+++ src/reducers/Search.js
@@ -3,23 +3,28 @@ import { initialSearchState } from "../store/Store";
 
 export default function(state = initialSearchState, action) {
   switch (action.type) {
+    case ActionTypes.SET_SEARCH_INPUT_TEXT:
+      return Object.assign({}, state, {
+        inputText: action.inputText
+      });
     case ActionTypes.START_SEARCH:
-      return {
+      return Object.assign({}, state, {
         isFetching: true,
         results: null
-      };
+      });
     case ActionTypes.RECEIVE_SEARCH_RESULTS:
       // Additionally, in the parcels reducer a parcel is created for each result
       // that doesn't have one yet.
-      return {
+      return Object.assign({}, state, {
         isFetching: false,
         results: action.results.map(result => result.id)
-      };
+      });
     case ActionTypes.CLEAR_SEARCH_RESULTS:
-      return {
+      return Object.assign({}, state, {
         isFetching: false,
-        results: null
-      };
+        results: null,
+        inputText: ""
+      });
     default:
       return state;
   }
