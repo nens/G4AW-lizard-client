570cfc5ea77f9574f77df3f22459a8a4ccecc488
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-19 12:01:30 +0200

    fix(): Replaced Object.assign()s with ...state destructurer

--- src/reducers/Search.js
+++ src/reducers/Search.js
@@ -4,27 +4,31 @@ import { initialSearchState } from "../store/Store";
 export default function(state = initialSearchState, action) {
   switch (action.type) {
     case ActionTypes.SET_SEARCH_INPUT_TEXT:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         inputText: action.inputText
-      });
+      };
     case ActionTypes.START_SEARCH:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: true,
         results: null
-      });
+      };
     case ActionTypes.RECEIVE_SEARCH_RESULTS:
       // Additionally, in the parcels reducer a parcel is created for each result
       // that doesn't have one yet.
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         results: action.results.map(result => result.id)
-      });
+      };
     case ActionTypes.CLEAR_SEARCH_RESULTS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         results: null,
         inputText: ""
-      });
+      };
     default:
       return state;
   }
