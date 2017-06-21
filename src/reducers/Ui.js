6ce688b9f8d321fc2d8e6d14e8861d27462e9630
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-21 14:35:18 +0200

    fix(SnackBar): Adds reducer for SnackBar hide/show.

--- src/reducers/Ui.js
+++ src/reducers/Ui.js
@@ -3,6 +3,10 @@ import { initialUiState } from "../store/Store";
 
 export default function(state = initialUiState, action) {
   switch (action.type) {
+    case ActionTypes.SHOW_SNACKBAR:
+      return { ...state, showSnackBar: true, snackBarOptions: action.options };
+    case ActionTypes.HIDE_SNACKBAR:
+      return { ...state, showSnackBar: false, snackBarOptions: {} };
     case ActionTypes.GET_ATTRIBUTES_FROM_GEOSERVER:
       return { ...state, selectedParcel: action.parcelId };
     case ActionTypes.CHANGE_VIEW:
