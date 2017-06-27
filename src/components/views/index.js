eb5d153f8d060d2f3fc6ee33fb50532c39feb0ea
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-06-27 14:13:34 +0200

    Add new index.js files

---
+++ src/components/views/index.js
@@ -0,0 +1,6 @@
+import DetailView from "./DetailView";
+import ListSearchView from "./ListSearchView";
+import MapSearchView from "./MapSearchView";
+import PhotoView from "./PhotoView";
+
+export { DetailView, ListSearchView, MapSearchView, PhotoView };
