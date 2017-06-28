ce82b7e885651f52d6ed4d0c08b20c41d7ee455b
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-06-28 15:43:43 +0200

    fix(): name of module changed

--- stories/SearchResultCardStories.jsx
+++ stories/SearchResultCardStories.jsx
@@ -2,7 +2,7 @@ import { storiesOf, action } from "@kadira/storybook";
 import { i18nDecorator } from "./helpers.jsx";
 import React from "react";
 
-import SearchResultCard from "../src/components/SearchResultCard";
+import SearchResultCardItem from "../src/components/SearchResultCardItem";
 
 const vnLongTitle = "Hàng Tiệc Cưới Hàng Tiệc Cưới";
 
@@ -16,7 +16,7 @@ function getComponent(kvargs = {}) {
   const getVal = key =>
     kvargs[key] !== undefined ? kvargs[key] : DEFAULTS[key];
   return (
-    <SearchResultCard
+    <SearchResultCardItem
       handleClick={action("Clicked!")}
       title={getVal("title")}
       subtitle={getVal("subtitle")}
@@ -26,8 +26,8 @@ function getComponent(kvargs = {}) {
   );
 }
 
-export default function runSearchResultCard() {
-  storiesOf("SearchResultCard", module)
+export default function runSearchResultCardItem() {
+  storiesOf("SearchResultCardItem", module)
     .addDecorator(i18nDecorator)
     .add("default result", () => getComponent())
     .add("long title", () => getComponent({ title: vnLongTitle }))
