e127fa7de9e09c6c4b235fdbdc1891cf682b86bb
d-weeteling <d-weeteling@users.noreply.github.com>
2017-07-20 10:08:46 +0200

    changed prop name (#132)

--- src/components/DetailViewSection.jsx
+++ src/components/DetailViewSection.jsx
@@ -21,7 +21,7 @@ export default class DetailViewSection extends Component {
   constructor() {
     super();
     this.state = {
-      isOpen: this.props && this.props.isOpen
+      isOpen: this.props && this.props.isInitiallyOpen
     };
     this.toggleSection = this.toggleSection.bind(this);
   }
@@ -32,14 +32,7 @@ export default class DetailViewSection extends Component {
     this.setState({ isOpen: !this.state.isOpen });
   }
   render() {
-    const {
-      children,
-      colorCode,
-      handleClick,
-      title,
-      subTitle,
-      isOpen
-    } = this.props;
+    const { children, colorCode, handleClick, title, subTitle } = this.props;
 
     return (
       <div className={styles.DetailViewSection}>
@@ -65,7 +58,7 @@ export default class DetailViewSection extends Component {
 DetailViewSection.propTypes = {
   handleClick: PropTypes.func,
   children: PropTypes.object,
-  open: PropTypes.bool,
+  isInitiallyOpen: PropTypes.bool,
   title: PropTypes.string,
   subtitle: PropTypes.string
 };
