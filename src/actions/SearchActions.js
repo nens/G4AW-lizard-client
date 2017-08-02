6a1bbb9699ab0ce1f934f51ca4b2c1a2e9b7eddb
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-08-02 10:24:30 +0200

    Only use bbox for search when in MapSearchView

--- src/actions/SearchActions.js
+++ src/actions/SearchActions.js
@@ -45,7 +45,7 @@ export function setSearchInputText(dispatch, inputText) {
   });
 }
 
-function doSearch(dispatch, q, types = null, exclude = []) {
+function doSearch(dispatch, q, spatializeSearch) {
   const state = theStore.getState();
   const currentData = state.search;
 
@@ -54,20 +54,35 @@ function doSearch(dispatch, q, types = null, exclude = []) {
     return;
   }
 
-  const searchCompliantBbox = [
-    state.map.bbox[3],
-    state.map.bbox[0],
-    state.map.bbox[1],
-    state.map.bbox[2]
-  ];
+  const searchCompliantBbox = spatializeSearch
+    ? [
+        state.map.bbox[3],
+        state.map.bbox[0],
+        state.map.bbox[1],
+        state.map.bbox[2]
+      ]
+    : null;
 
   startSearch(dispatch);
   searchParcels(q, searchCompliantBbox).then(
     results => {
       receiveResultsSuccess(dispatch, results);
-      const parcels = results.map(r => feature(r.geometry));
-      const boundingBox = bbox(flip(featureCollection(parcels)));
-      updateMapBbox(dispatch, boundingBox);
+      if (results.length > 0) {
+        // Only update bbox if there were actual results returned:
+        const parcels = results.map(r => feature(r.geometry));
+        const boundingBox = bbox(flip(featureCollection(parcels)));
+        updateMapBbox(dispatch, boundingBox);
+      } else {
+        showSnackBar(dispatch, {
+          autoHideDuration: 4000,
+          isError: true,
+          message: `${i18next.t("No search results found for")} '${q}'
+            ${i18next.t("in your current extent")}`,
+          subMessage: i18next.t(
+            "Please try searching elsewhere/using a different query"
+          )
+        });
+      }
     },
     error => {
       const msg = "Search error: " + error;
