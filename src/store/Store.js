78eb075a76164a80a4f10ff51023e87a341f43c1
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-08-09 16:09:55 +0200

    fix(Legend): Add two farmer classes for Rice legend.

--- src/store/Store.js
+++ src/store/Store.js
@@ -115,7 +115,7 @@ export const initialForegroundlayerState = {
       slug: "latest_weekly_LT",
       mapThumb:
         "https://maps1.project.lizard.net/geoserver/Q0007_sat4rice_2018/wms?service=WMS&request=GetMap&layers=latest_weekly_LT&styles=&format=image%2Fpng&transparent=True&version=1.1.1&height=256&width=256&srs=EPSG%3A3857&bbox=11709388.36300615,1164288.814839805,11709541.23706272,1164441.6888963755",
-      colormap: []
+      colormap: [{ LocTroi: "#000000" }, { Other: "#B6AEA8" }]
     },
     {
       title: i18next.t("Growth stages"),
