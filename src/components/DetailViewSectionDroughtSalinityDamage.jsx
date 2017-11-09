f66165cbf8494ab124ea5f74521564019cf9e5b9
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-11-09 12:55:29 +0100

    rm comments

--- src/components/DetailViewSectionDroughtSalinityDamage.jsx
+++ src/components/DetailViewSectionDroughtSalinityDamage.jsx
@@ -4,27 +4,8 @@ import { SECTIONS, NO_DATA } from "../constants/detailview-attributes";
 import { DetailViewSection, DetailViewTable } from ".";
 
 export default class DetailViewSectionDroughtSalinityDamage extends Component {
-  // getTranslatedRisk(t, parcel) {
-  //   if (parcel.FloodRisk === undefined) {
-  //     return t("unknown");
-  //   } else {
-  //     return parcel.FloodRisk.toUpperCase();
-  //   }
-  // }
-  // getSubTitleText(t, parcel, translatedRisk) {
-  //   const floodLevelValue =
-  //     parcel.FloodLevel !== undefined ? parcel.FloodLevel : t("unknown");
-  //   return [
-  //     t("The current flood risk is"),
-  //     translatedRisk,
-  //     t("and the flood level is"),
-  //     floodLevelValue,
-  //     SECTIONS.FloodRisk.sectionAttrs[1].getTranslatedUnit()
-  //   ].join(" ");
-  // }
   render() {
     const { t, parcel, data } = this.props;
-    // const translatedRisk = this.getTranslatedRisk(t, parcel);
     return (
       <DetailViewSection
         isInitiallyOpen={false}
